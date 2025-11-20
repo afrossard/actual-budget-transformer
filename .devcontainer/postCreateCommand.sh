e05a02e91570bbf97ba5c1b542760164cb1d942a
afrossard <aurelien.frossard@gmail.com>
2025-11-20 21:20:15 +0000

    Replaced poetry with uv in devcontainer

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 cp ./.devcontainer/.bash_aliases ~/
-pipx install poetry==2.1.3
-poetry completions bash | sudo tee /etc/bash_completion.d/poetry
-poetry install
+pipx install uv==0.9.10
+echo 'eval "$(uv generate-shell-completion bash)"' | sudo tee /etc/bash_completion.d/uv
+echo 'eval "$(uvx --generate-shell-completion bash)"'  | sudo tee /etc/bash_completion.d/uvx
+uv sync
