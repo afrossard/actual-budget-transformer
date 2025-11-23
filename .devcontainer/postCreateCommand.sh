eb54e76b89bd5906095a0f5ca117b6e1f87ce395
afrossard <aurelien.frossard@gmail.com>
2025-11-23 15:14:28 +0000

    Dev conainer updates for uv

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -3,4 +3,4 @@ cp ./.devcontainer/.bash_aliases ~/
 pipx install uv==0.9.10
 echo 'eval "$(uv generate-shell-completion bash)"' | sudo tee /etc/bash_completion.d/uv
 echo 'eval "$(uvx --generate-shell-completion bash)"'  | sudo tee /etc/bash_completion.d/uvx
-uv sync
+UV_LINK_MODE=copy uv sync
